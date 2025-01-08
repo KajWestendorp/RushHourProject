8428c88463e994174acb402ab22a02d7866df0b2
Kaj Westendorp <k.westendorp@student.uva.nl>
2025-01-08 11:37:37 +0100

    Test breedte

--- code/classes/__init__.py
+++ code/classes/__init__.py
@@ -1,8 +1,9 @@
 class Voertuig():
-    def __init__(self, positie, lengte, orientatie):
+    def __init__(self, positie, lengte, orientatie, breedte):
         self.positie = positie
         self.lengte = lengte
         self.richting = orientatie
+        self.breedte = breedte
 
     def bewegen(self, positie, lengte, orientatie):
         pass
