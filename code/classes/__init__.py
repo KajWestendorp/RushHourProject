623f54aac0847a14381ed6c07551f968ef99e348
18pieter16 <pieter.wassink@gmail.com>
2025-01-08 11:31:54 +0100

    push test self.richting

--- code/classes/__init__.py
+++ code/classes/__init__.py
@@ -2,6 +2,7 @@ class Voertuig():
     def __init__(self, positie, lengte, orientatie):
         self.positie = positie
         self.lengte = lengte
+        self.richting = orientatie
 
     def bewegen(self, positie, lengte, orientatie):
         pass
