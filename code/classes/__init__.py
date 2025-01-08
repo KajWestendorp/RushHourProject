b6f4936ed62ad3c83ea82bebb9824903b046c6b9
18pieter16 <pieter.wassink@gmail.com>
2025-01-08 11:25:49 +0100

    Code + Classes folder aangemaakt + init py begonnen

---
+++ code/classes/__init__.py
@@ -0,0 +1,10 @@
+class Voertuig():
+    def __init__(self, positie, lengte, orientatie):
+        self.positie = positie
+        self.lengte = lengte
+        self.orientatie = orientatie
+
+    def bewegen(self, positie, lengte, orientatie):
+        pass
+
+
