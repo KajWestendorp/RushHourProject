5a4e99ee08c6160085155b6b31cc4bdb10e2b607
Aedan Soellaart <aedansoellaart@gmail.com>
2025-01-08 11:30:21 +0100

    Removed orientatie (test)

--- code/classes/__init__.py
+++ code/classes/__init__.py
@@ -2,7 +2,6 @@ class Voertuig():
     def __init__(self, positie, lengte, orientatie):
         self.positie = positie
         self.lengte = lengte
-        self.orientatie = orientatie
 
     def bewegen(self, positie, lengte, orientatie):
         pass
