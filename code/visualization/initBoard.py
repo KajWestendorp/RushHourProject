2200f17baecec458c1fe8f728ff7a349ed7c7497
18pieter16 <pieter.wassink@gmail.com>
2025-01-08 16:01:29 +0100

    Begin bord visualisatie

---
+++ code/visualization/initBoard.py
@@ -0,0 +1,9 @@
+# Dit script maakt een initiele visualisatie van het bord en de autos, op basis van een gegeven csv file.
+
+import pandas as pd
+
+
+
+bordposities = pd.read_csv('Rushhour6x6_1.csv', 'r', encoding='utf-8')
+
+
