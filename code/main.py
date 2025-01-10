ffd090fb160007e0cf6bb771aecf791d103b0d9f
18pieter16 <pieter.wassink@gmail.com>
2025-01-10 11:55:49 +0100

    fixed main.py os directory

--- code/main.py
+++ code/main.py
@@ -5,17 +5,18 @@ import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 from movement.updatedf import update_positions
 from visualization.initBoard import visualize_board
+import os
 
 
 if __name__ == "__main__":
-    # script_dir = os.path.dirname(os.path.abspath(__file__))
-    # relative_path = os.path.join("..", "gameboards", "Rushhour6x6_1.csv")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    relative_path = os.path.join("gameboards", "Rushhour6x6_1.csv")
 
-    # # Construct the path to the gameboard file
-    # board_file = os.path.normpath(os.path.join(script_dir, relative_path))
+    # Construct the path to the gameboard file
+    board_file = os.path.normpath(os.path.join(script_dir, relative_path))
 
     #for now boardfile hardcoded cuz errors 
-    board_file = "C:/Users/Weste/Documents/MinorAI/Algos/RushHourProject/code/gameboards/Rushhour6x6_1.csv"
+    #board_file = "C:/Users/Weste/Documents/MinorAI/Algos/RushHourProject/code/gameboards/Rushhour6x6_1.csv"
     
     boardposition1 = pd.read_csv(board_file, sep=',', encoding='utf-8')
 
