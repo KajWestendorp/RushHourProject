3e023c6c4399bf6dadd8636bf72108eef0a8a54e
Kaj Westendorp <kaj.westendorp@student.uva.nl>
2025-01-10 11:15:18 +0100

    fixing main

--- code/main.py
+++ code/main.py
@@ -5,21 +5,21 @@ from movement.updatedf import update_positions
 from visualization.initBoard import visualize_board
 
 
-# if __name__ == "__main__":
-#     script_dir = os.path.dirname(os.path.abspath(__file__))
-#     relative_path = os.path.join("..", "gameboards", "Rushhour6x6_1.csv")
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    relative_path = os.path.join("..", "gameboards", "Rushhour6x6_1.csv")
 
-#     # Construct the path to the gameboard file
-#     board_file = os.path.normpath(os.path.join(script_dir, relative_path))
+    # Construct the path to the gameboard file
+    board_file = os.path.normpath(os.path.join(script_dir, relative_path))
 
-#     boardposition1 = pd.read_csv(board_file, sep=',', encoding='utf-8')
+    boardposition1 = pd.read_csv(board_file, sep=',', encoding='utf-8')
 
-    # print(boardposition1)
-    # board = visualize_board(boardposition1)
-    # print(board)
+    print(boardposition1)
+    board = visualize_board(boardposition1)
 
-    # print("yes")
-    # newdf = update_positions(boardposition1, 2, 'X')
-    # print(newdf)
+    newboarddf = update_positions(boardposition1, 2, 'X')
+    print(newboarddf)
+
+    newboard = visualize_board(newboarddf)
 
 
