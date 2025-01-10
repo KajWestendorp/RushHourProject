cc48d8f9d5db4a6ba15c854c9bb8b9c318702adb
Kaj Westendorp <kaj.westendorp@student.uva.nl>
2025-01-10 11:57:13 +0100

    removed close

--- code/main.py
+++ code/main.py
@@ -15,20 +15,19 @@ if __name__ == "__main__":
     # board_file = os.path.normpath(os.path.join(script_dir, relative_path))
 
     #for now boardfile hardcoded cuz errors 
-    board_file = "C:/Users/Weste/Documents/MinorAI/Algos/RushHourProject/code/gameboards/Rushhour6x6_1.csv"
+    board_file = "./gameboards/Rushhour6x6_1.csv"
     
     boardposition1 = pd.read_csv(board_file, sep=',', encoding='utf-8')
 
-    visualize_board(boardposition1)
+    figure1 = visualize_board(boardposition1)
 
     newboarddf = update_positions(boardposition1, -1, 'A')
 
-    visualize_board(newboarddf)
+    figure2 = visualize_board(newboarddf)
 
     #Made it so that the plots show 2 seconds after eachother making a sort of stop motion
     plt.show(block = False)
     plt.pause(2)
-    plt.close()
     plt.show()
 
 
