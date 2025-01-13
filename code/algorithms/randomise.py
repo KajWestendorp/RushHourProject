b5569ce7b2a9d66037d1eb9e56cb207cfccdbbdf
Kaj Westendorp <kaj.westendorp@student.uva.nl>
2025-01-13 17:36:12 +0100

    Added comments to randomise.py

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -10,8 +10,12 @@ def is_valid(coords):
     dictionary
 
     """
+
+    #create a set to store the seen coordinates into 
     coord_set = set()
 
+    # go through the items in the dictionary and check if it is in the set and if it is not we add it as a tuple 
+    # Use a tuple so that we can add it to the set
     for car, coordinates in coords.items():
         for coordinate in coordinates:
             if coordinate in coord_set:
@@ -25,10 +29,16 @@ def create_coords(boarddf):
     Function that uses the col, orientation and row to create coordinates column for each car and add it to the df
     
     """
+
+    #initialize the dict
     coords = {}
     
     # https://stackoverflow.com/questions/16031056/how-to-form-tuple-column-from-two-columns-in-pandas
     for _, car in boarddf.iterrows():
+
+        # Checks the orientation of the car and adjusts the column by 1 to include the space above it and row plus 1 for vertical cars
+
+        # TODO: WIll need to add a check for car length later on to ensure the corret amount is being added to the coords for trucks
         if car['orientation'] == 'H':
             coords[car['car']] = ((car['col'], car['row']), (car['col'] + 1, car['row']))
         else:
@@ -37,9 +47,13 @@ def create_coords(boarddf):
     return coords
 
 def random_car(boarddf):
+
+    # Takes a random sample from the cars that are available
     return boarddf['car'].sample(1).iloc[0]
 
 def finish_check(boarddf):
+
+    # Checks whether the red car has reached an exit
     if boarddf.loc[boarddf['car'] == 'X', 'col'].iloc[0] == 5:
         return False
     return True
