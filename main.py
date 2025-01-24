a9ee835d833ca3ca9f1e857ba1dba780bc2b9523
Kaj Westendorp <kaj.westendorp@student.uva.nl>
2025-01-24 14:53:34 +0100

    Added 9x9 boardfiles

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     relative_path3 = os.path.join("code", "gameboards", "Rushhour6x6_test.csv")
     relative_path4 = os.path.join("code", "gameboards", "Rushhour6x6_2.csv")
     relative_path5 = os.path.join("code", "gameboards", "Rushhour6x6_3.csv")
-    relative_path6 = os.path.join("code", "gameboards", "Rushhour12x12_7.csv")
+    relative_path9x9_5 = os.path.join("code", "gameboards", "Rushhour9x9_5.csv")
+    relative_path9x9_6 = os.path.join("code", "gameboards", "Rushhour9x9_6.csv")
 
     # Construct the path to the gameboard file
     board_file = os.path.normpath(os.path.join(script_dir, relative_path))
@@ -34,7 +35,8 @@ if __name__ == "__main__":
     board_file3 = os.path.normpath(os.path.join(script_dir, relative_path3))
     board_file4 = os.path.normpath(os.path.join(script_dir, relative_path4))
     board_file5 = os.path.normpath(os.path.join(script_dir, relative_path5))
-    board_file6 = os.path.normpath(os.path.join(script_dir, relative_path6))
+    board_file9x9_5 = os.path.normpath(os.path.join(script_dir, relative_path9x9_5))
+    board_file9x9_6 = os.path.normpath(os.path.join(script_dir, relative_path9x9_6))
 
     # visualize_board(boardposition1, 6)
     boardposition1 = pd.read_csv(board_file, sep=',', encoding='utf-8')
@@ -42,7 +44,9 @@ if __name__ == "__main__":
     boardposition3 = pd.read_csv(board_file3, sep=',', encoding='utf-8')
     boardposition4 = pd.read_csv(board_file4, sep=',', encoding='utf-8')
     boardposition5 = pd.read_csv(board_file5, sep=',', encoding='utf-8')
-    boardposition6 = pd.read_csv(board_file6, sep=',', encoding='utf-8')
+    boardposition9x9_5 = pd.read_csv(board_file9x9_5, sep=',', encoding='utf-8')
+    boardposition9x9_6 = pd.read_csv(board_file9x9_5, sep=',', encoding='utf-8')
+
 
     """----- Random Algorithm -----"""
     # Number of trials
@@ -220,13 +224,13 @@ if __name__ == "__main__":
 initial_grid = Grid(12)
 initial_grid.create_grid()
 initial_grid.add_borders()
-initial_grid.add_cars_to_board(boardposition2)
-
+initial_grid.add_cars_to_board(boardposition6)
 #Run algorithm
 import time
 
 # Calculate the start time
 start = time.time()
+
 final_grid, outputdf = breadth_first(initial_grid)
 # Calculate the end time and time taken
 end = time.time()
