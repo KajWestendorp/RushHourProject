863bcbdcfe5883e4cd20ddaf1410eae9cb091118
Kaj Westendorp <kaj.westendorp@student.uva.nl>
2025-01-29 13:21:31 +0100

    Moved visualize BFS to correct folder

--- code/visualization/Visualize_BFS.py
+++ code/visualization/Visualize_BFS.py
@@ -24,7 +24,7 @@ plt.ylabel('Move Counts')
 # Show the plot
 plt.show()
 
-
+# Create lists
 list_of_boards = ['Random','Hillclimber','BFS']
 move_counts = [336, 181, 15]
 
@@ -39,12 +39,13 @@ sns.barplot(x='Board', y='Move Counts', data=data, palette='viridis')
 
 # Add labels and title
 plt.title('Move Counts for 6x6 Board #1')
-plt.xlabel('Board')
-plt.ylabel('Move Counts')
+plt.xlabel('Board', fontsize = 30)
+plt.ylabel('Move Counts', fontsize = 25)
 
 # Show the plot
 plt.show()
 
+# Create lists
 list_of_boards = ['Random','Hillclimber','BFS']
 move_counts = [1749, 1595, 27]
 
